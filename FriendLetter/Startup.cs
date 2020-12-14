5dd8beaff73c8f977abf840710f1173260ae1673
Tawnee Harris <tawneeh@icloud.com>
2020-12-13 22:29:59 -0800

    add method to report if Razor fails to load

--- FriendLetter/Startup.cs
+++ FriendLetter/Startup.cs
@@ -36,6 +36,8 @@ namespace FriendLetter
       {
         await context.Response.WriteAsync("Hello World!");
       });
+
+      app.UseDeveloperExceptionPage(); // produces error report when Razor fails to load
     }
 
   }
